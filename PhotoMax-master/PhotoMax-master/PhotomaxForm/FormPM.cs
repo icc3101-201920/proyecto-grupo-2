6b63a6f523da8a9711bd431e6ec37b52625b9924
unknown <fjmiranda4iuandes.cl>
2019-10-28 11:22:23 -0300

    instrucciones dentro

--- PhotoMax-master/PhotoMax-master/PhotomaxForm/FormPM.cs
+++ PhotoMax-master/PhotoMax-master/PhotomaxForm/FormPM.cs
@@ -8,10 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace PhotomaxForm
 {
     public partial class FormPM : Form
     {
+        // Para ir haciendo el forms del programa:
+
+        //1)La gracia es que solo debe haber 1 forms y mientras se van haciendo clicks en botones ir "navegando" en los
+        //paneles
+
+        //2)Los nombres de los diferente eventos del toolbar tienen numero, ese numero es perteneciente al numero del paner en el
+        //que están puesto
+
+        //EJ: En mi panel 1 tendré botones/barras/label/ lo que sea, estos tienen el nombre respectivo de la herramienta
+
+        //    el NUMERO al panel al que pertenecen y la accion correspondiente
+
+        //3)Cuando hago click en un boton para proceder a una accion siguiente tal como "aplicar filtro" --> "sepia" --> "guardar"
+        //es cuando se cambian los panel
+
+        //EJ: en el caso anterior pasaríamos del panel 6 --> 7 --> 8. 
+
+        
         public FormPM()
         {
             InitializeComponent();
@@ -54,6 +74,15 @@ namespace PhotomaxForm
 
         private void CheckBox2option3_CheckedChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void BtnNext2_Click(object sender, EventArgs e)
+        {
+            // En next tengo que pasar al siguiente menu dependiendo la opción que yo elegí
+            //en los checkbox anteriores
+
+            
 
         }
     }
